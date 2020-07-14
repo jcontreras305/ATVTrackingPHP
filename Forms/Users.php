08f0e55800ccc064e2d0b715113141d2e4afcd19
jcontreras305 <38672355+jcontreras305@users.noreply.github.com>
2020-07-13 19:31:47 -0500

    Ejemplos de paginas secundarias

---
+++ Forms/Users.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div>
+            <a><h2>ATVTRAKING</h2></a> 
+            <br>
+            <a><h3>Users</h3></a>
+        </div>
+        <div>
+            <form action="Users.php" method="POST">
+                <ul>
+                    <li><input type="submit" value="Employees" name="btnEmployees"></li>
+                    <li><input type="submit" value="Materials" name="btnMaterials "></li>
+                    <li><input type="submit" value="Customers" name="btnCustomers"></li>
+                    <li><input type="submit" value="Proyects" name="btnProjects"></li>
+                    <li><input type="submit" value="Users" name="btnUsers"></li>
+                </ul>
+            </form>
+            <?php
+                if (isset($_POST["btnEmployees"])){
+                    header ("Location: ./Employees.php");
+                }
+                elseif (isset($_POST["btnMaterials"])) {
+                    header ("Location: ./Materials.php");
+                }
+                elseif (isset($_POST["btnCustomers"])) {
+                    header ("Location: ./Customers.php");
+                }
+                elseif (isset($_POST["btnProyects"])) {
+                    header ("Location: ./Projects.php");
+                }
+                elseif (isset($_POST["btnUsers"])) {
+                    header ("Location: ./Users.php");
+                }
+            ?>
+        </div>
+    </body>
+</html>
